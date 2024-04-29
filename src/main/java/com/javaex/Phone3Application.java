f19cdf88b5ebc65d7503be3be88982018c7f08c9
yss9358 <93y2s@naver.com>
2024-04-29 18:50:13 +0900

    master

---
+++ src/main/java/com/javaex/Phone3Application.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phone3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phone3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaex/Phone3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phone3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
