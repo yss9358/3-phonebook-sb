d50f1bb1355d357478e3e7a417c2747c1cd37253
yss9358 <93y2s@naver.com>
2024-04-30 14:51:19 +0900

    test

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -3,14 +3,14 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhoneService;
 import com.javaex.util.JsonResult;
 import com.javaex.vo.PersonVo;
 
-@Controller
+@RestController
 public class PhoneController {
 
 	@Autowired
