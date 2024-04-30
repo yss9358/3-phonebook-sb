fb10cb1395328a39c23789f22f8421f5173de984
ImSol-k <tiq0015@naver.com>
2024-04-30 15:03:27 +0900

    소리 테스트

---
+++ src/main/java/com/javaex/controller/SolListController.java
@@ -0,0 +1,18 @@
+package com.javaex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaex.util.JsonResult;
+
+@RestController
+@RequestMapping("phone3/list")
+public class SolListController {
+
+	@GetMapping("main")
+	public JsonResult mainList() {
+		System.out.println("SolListController.mainList()");
+		return JsonResult.success("데이터보내기");
+	}
+}
