348724ae99893b26f163f1a84d14aa2dae99308d
yss9358 <93y2s@naver.com>
2024-05-01 15:39:13 +0900

    master

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -1,9 +1,14 @@
 package com.javaex.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhoneService;
@@ -17,6 +22,8 @@ public class PhoneController {
 	@Autowired
 	private PhoneService phoneService;
 	
+	
+	// 전체 리스트 불러오기
 	@GetMapping("/phone3/list")
 	public JsonResult list() {
 		List<PersonVo> list = phoneService.exeList();
@@ -27,9 +34,8 @@ public class PhoneController {
 		}
 	}
 
-	
-	
-	@GetMapping("/phone3/team")
+	// 그룹 리스트 불러오기 
+	@GetMapping("/phone3/teams")
 	public JsonResult teamlist() {
 		List<TeamVo> list = phoneService.exeTeamList();
 		if(list != null) {
@@ -38,5 +44,29 @@ public class PhoneController {
 			return JsonResult.fail("리스트를 불러오는데 실패했습니다.");
 		}
 	}
+	
+	// 그룹 추가 
+	@PostMapping("/phone3/teams")
+	public JsonResult addTeam(@RequestBody Map<String, String> map) {
+		String teamName = map.get("teamName");
+		int count = phoneService.exeInsertTeam(teamName);
+		if(count == 1) {
+			return JsonResult.success(count);
+		} else {
+			return JsonResult.fail("그룹명을 변경해주세요.");
+		}
+	}
+	
+	// 그룹 삭제
+	@DeleteMapping("/phone3/teams/{no}")
+	public JsonResult delete(@PathVariable int no) {
+		int count = phoneService.exeDelete(no);
+		if(count == 1) {
+			return JsonResult.success(count);
+		} else {
+			return JsonResult.fail("데이터 오류");
+		}
+	}
+	
 	
 }
--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -15,12 +15,35 @@ public class PhoneDao {
 	@Autowired
 	private SqlSession sqlSession;
 
+	// 전체 리스트 불러오기
 	public List<PersonVo> list() {
 		return sqlSession.selectList("phone.list");
 	}
 	
+	// 그룹 리스트 불러오기
 	public List<TeamVo> teamList() {
 		return sqlSession.selectList("phone.teamList");
 	}
 	
+	// 그룹 추가
+	public int insertTeam(String teamName) {
+		return sqlSession.insert("phone.insertTeam", teamName);
+	}
+	
+	// 그룹 중복 확인
+	public TeamVo teamCheck(String teamName) {
+		return sqlSession.selectOne("phone.teamCheck",teamName);
+	}
+	
+	// 그룹 기본값으로 변경
+	public int changeTeam(int no) {
+		int count = sqlSession.update("phone.changeTeam", no);
+		return count;
+	}
+	
+	// 그룹 삭제
+	public int deleteTeam(int no) {
+		int count = sqlSession.delete("phone.deleteTeam",no);
+		return count;
+	}
 }
