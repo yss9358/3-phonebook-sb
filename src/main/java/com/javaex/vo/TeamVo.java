e8817ef39c1fca53448f814ec12242b7829ca523
yss9358 <93y2s@naver.com>
2024-05-01 11:35:26 +0900

    mapper 추가

--- src/main/java/com/javaex/vo/TeamVo.java
+++ src/main/java/com/javaex/vo/TeamVo.java
@@ -4,30 +4,45 @@ public class TeamVo {
 
 	private int teamNo;
 	private String teamName;
-	
+	private int count;
 	
 	public TeamVo() {
 		
 	}
-	public TeamVo(int teamNo, String teamName) {
+
+	public TeamVo(int teamNo, String teamName, int count) {
 		this.teamNo = teamNo;
 		this.teamName = teamName;
+		this.count = count;
 	}
+
 	public int getTeamNo() {
 		return teamNo;
 	}
+
 	public void setTeamNo(int teamNo) {
 		this.teamNo = teamNo;
 	}
+
 	public String getTeamName() {
 		return teamName;
 	}
+
 	public void setTeamName(String teamName) {
 		this.teamName = teamName;
 	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
 	@Override
 	public String toString() {
-		return "TeamVo [teamNo=" + teamNo + ", teamName=" + teamName + "]";
+		return "TeamVo [teamNo=" + teamNo + ", teamName=" + teamName + ", count=" + count + "]";
 	}
 	
 	
