e4222c1ace4c5cb9a42bdb49c521886836246ef7
m-ji97 <tv5035@naver.com>
2024-06-04 14:50:49 +0900

    mj

--- src/main/java/com/javaex/controller/DeleteController.java
+++ src/main/java/com/javaex/controller/DeleteController.java
@@ -1,10 +1,8 @@
 package com.javaex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.DeleteService;
--- src/main/java/com/javaex/dao/DeleteDao.java
+++ src/main/java/com/javaex/dao/DeleteDao.java
@@ -4,8 +4,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.javaex.vo.PersonVo;
-
 @Repository
 public class DeleteDao {
 	
